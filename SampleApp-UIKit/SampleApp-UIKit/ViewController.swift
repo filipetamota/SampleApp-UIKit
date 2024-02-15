ea2a05e6ba1aba1cbea9d81305040209b02bb9f0
filipetamota <filipetamota@gmail.com>
2024-02-15 12:48:20 +0100

    Project created

---
+++ SampleApp-UIKit/SampleApp-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApp-UIKit
+//
+//  Created by Filipe Mota on 15/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
